3e71c400ee43c2f6f99a2df5e2712285f457d9bb
stach231 <adam.a.stach@gmail.com>
2024-02-17 12:35:07 +0100

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div id="title">
+        <label>Cogisum</label>  
+      </div>     
+    </>
+  )
+}
+
+export default App
